6e097208aa1f7e5afa3f230c0202cbe174467adb
Arisha <arishaaa333@gmail.com>
2023-11-16 19:43:30 +0530

    props in react

---
+++ src/Components/Jokes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Jokes = (props) => {
+    console.log(props.upvotes)
+    console.log(props.isPun)
+  return (
+    <>
+    <div className='jokes'>
+    {props.setup && <h2>Setup: {props.setup}</h2>}
+    <h4>Punchline: {props.punchline}</h4>
+    </div>
+    <hr />
+    </>
+  )
+}
